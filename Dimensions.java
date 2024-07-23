4710a84188877e7fd297b0d5a7038a779df1e572
WilliamMcKeown1 <william.mckeown08@gmail.com>
2024-07-23 19:18:51 -0400

    Add files via upload

---
+++ Dimensions.java
@@ -0,0 +1,142 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.*;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Dimensions implements ActionListener {
+    private static final int BUTTON_HEIGHT = 75;
+    private static final int BUTTON_WIDTH = 400;
+    private static final int TEXTFIELD_HEIGHT = 50;
+    private static final int TEXTFIELD_WIDTH = 400;
+    private static final int HEIGHT = 800;
+    private static final int WIDTH = 800;
+
+    private boolean hidden;
+
+    private JFrame f;
+    private JTextField t1;
+    private JTextField t2;
+    private JLabel tp;
+    private JButton submit;
+    private JButton hide;
+    private JPanel panel;
+    private GridPanel gridPanel;
+
+    public Dimensions() {
+        f = new JFrame();
+        f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+        f.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension bd = new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT);
+        Dimension td = new Dimension(TEXTFIELD_WIDTH, TEXTFIELD_HEIGHT);
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        t1 = new JTextField(10);
+        t1.setPreferredSize(td);
+        t1.setHorizontalAlignment(JTextField.CENTER);
+        t1.setMaximumSize(td);
+        t1.setAlignmentX(JTextField.CENTER_ALIGNMENT);
+        panel.add(t1);
+
+        t2 = new JTextField(10);
+        t2.setPreferredSize(td);
+        t2.setHorizontalAlignment(JTextField.CENTER);
+        t2.setMaximumSize(td);
+        t2.setAlignmentX(JTextField.CENTER_ALIGNMENT);
+        panel.add(t2);
+
+        tp = new JLabel();
+        tp.setPreferredSize(td);
+        tp.setHorizontalAlignment(JTextField.CENTER);
+        tp.setMaximumSize(td);
+        tp.setAlignmentX(JTextField.CENTER_ALIGNMENT);
+
+        submit = new JButton("Apply dimension");
+        submit.setActionCommand("submit");
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tp.setText("<html>Width: " + t1.getText() + "<br>Height: " + t2.getText() + "</html>");
+                tp.setFont(new Font("Verdana", Font.PLAIN, 14));
+                int x = Integer.parseInt(t1.getText());
+                int y = Integer.parseInt(t2.getText());
+                gridPanel.setDimensions(x, y);
+                gridPanel.repaint();
+                f.revalidate();
+            }
+        });
+        submit.setPreferredSize(bd);
+        submit.setMaximumSize(bd);
+        submit.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        submit.setBackground(Color.WHITE);
+        panel.add(submit);
+        f.add(panel);
+        hide = new JButton("Toggle Visibility");
+        hide.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hidden = !hidden;
+                panel.setVisible(!hidden);
+                tp.setVisible(!hidden);
+                f.revalidate();
+                f.repaint();
+            }
+        });
+        hide.setPreferredSize(new Dimension(150, 30));
+        hide.setMaximumSize(new Dimension(150, 30));
+        hide.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        f.add(hide);
+        f.add(tp);
+
+        gridPanel = new GridPanel();
+        gridPanel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        f.add(gridPanel);
+
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // Nothing :)
+    }
+
+    class GridPanel extends JPanel {
+        private int width;
+        private int height;
+
+        public void setDimensions(int width, int height) {
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (width > 0 && height > 0) {
+                int centerX = getWidth() / 2;
+                int centerY = getHeight() / 2;
+                int startX = centerX - (width / 2) * 3;
+                int startY = centerY - (height / 2) * 3;
+
+                for (int i = 0; i < width; i++) {
+                    for (int j = 0; j < height; j++) {
+                        g.fillRect(startX + i * 3, startY + j * 3, 3, 3);
+                        g.setColor(new Color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256)));
+                    }
+                }
+            }
+        }
+    }
+}
